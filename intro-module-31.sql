3ae8e0c2163867311d9b9a10e0e694d18c3bebe9
unknown <md.asif.8393@gmail.com>
2024-01-24 14:15:46 +0600

    [Done--> Create employee table and department table]

--- intro-module-31.sql
+++ intro-module-31.sql
@@ -1,1 +1,30 @@
-select * from user1
+
+-- Department Table
+-- Each department has many employees
+create table Department(
+    deptID SERIAL PRIMARY KEY,
+    deptName VARCHAR(50)
+);
+
+insert into Department
+    values (1, 'IT');
+
+DELETE From Department 
+    where deptID = 1;
+
+-- Employee TABLE
+-- Each employee belongs to a department
+create table Employee(
+    empID SERIAL PRIMARY KEY,
+    empName VARCHAR(50) NOT NULL,
+    departmentID INT, -- Foreign Key
+    CONSTRAINT fk_constraint_department -- work ON DELETE & UPDATE
+        FOREIGN KEY (departmentID) -- foreign key column name
+        REFERENCES Department(deptID) -- reference table and column
+);
+
+insert into Employee
+    values (1, 'Muktadir', 1);
+
+select * from Employee;
+
